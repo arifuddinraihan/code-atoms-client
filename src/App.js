1a79237672b45a6b072f0d3cf369bf8dbc07f8eb
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-24 22:25:24 +0600

    first commit- added router, tailwind, daisyUI theme, icon, title, toast, google font, react-icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>This will become a <code>Code Learning Website</code></h2>
+      <div className="btn btn-primary">Press Here</div>
+      <div className="btn btn-secondary">Press Here</div>
+      <div className="btn btn-accent">Press Here</div>
+      <div className="btn btn-warning">Press Here</div>
+      <div className="btn btn-success">Press Here</div>
+      <div className="btn btn-error">Press Here</div>
     </div>
   );
 }
