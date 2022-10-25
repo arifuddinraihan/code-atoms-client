dfaee3e499bdd4ecfbc3eb9c00985c8288711f4a
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-25 08:46:30 +0600

    added login register faq blog page with dark mode

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog/Blog";
+import Faq from "../components/FAQ/Faq";
 import Main from "../Layout/Main";
+import Login from "../Login/Login";
+import Register from "../Login/Register";
 
 export const routes = createBrowserRouter([
     {
@@ -11,11 +14,20 @@ export const routes = createBrowserRouter([
                 path : '/courses'
             },
             {
-                path : '/faq'
+                path : '/faq',
+                element : <Faq></Faq>
             },
             {
                 path : '/blog',
                 element : <Blog></Blog>
+            },
+            {
+                path : '/login',
+                element : <Login></Login>
+            },
+            {
+                path : '/register',
+                element : <Register></Register>
             }
         ]
     }
--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -24,20 +24,20 @@ const Navbar = () => {
         setTheme(theme === "dark" ? "light" : "dark");
     };
     return (
-        <div className="navbar bg-success transition-colors duration-300 transform dark:bg-white dark:hover:bg-gray-800 dark:hover:text-white shadow-xl sticky top-0 z-20 ">
+        <div className="navbar bg-success transition-colors duration-300 transform dark:bg-white dark:hover:bg-gray-800 dark:hover:text-white shadow-xl sticky top-0 z-40">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 gap-2">
-                        <li><NavLink to={'/courses'} className={({ isActive }) => isActive ? 'bg-success font-bold' : undefined}>Courses</NavLink></li>
-                        <li><NavLink to={'/faq'} className={({ isActive }) => isActive ? 'bg-success font-bold' : undefined}>FAQ</NavLink></li>
-                        <li><NavLink to={'/blog'} className={({ isActive }) => isActive ? 'bg-success font-bold' : undefined}>Blog</NavLink></li>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 dark:bg-slate-800 rounded-box w-52 gap-2">
+                        <li><NavLink to={'/courses'} className={({ isActive }) => isActive ? 'bg-success dark:bg-success font-bold' : undefined}>Courses</NavLink></li>
+                        <li><NavLink to={'/faq'} className={({ isActive }) => isActive ? 'bg-success dark:bg-success font-bold' : undefined}>FAQ</NavLink></li>
+                        <li><NavLink to={'/blog'} className={({ isActive }) => isActive ? 'bg-success dark:bg-success font-bold' : undefined}>Blog</NavLink></li>
                     </ul>
                 </div>
                 <Link to={'/'}
-                    className="ml-3 transition-colors duration-300 transform text-gray-800 hover:text-white dark:text-success dark:hover:text-gray-800">
+                    className="ml-3 transition-colors duration-300 transform text-gray-800 hover:text-white dark:text-success dark:hover:text-white">
                     <div className='flex justify-start text-xs items-center gap-2'>
                         <img className='hidden md:block' src="code-atom-icon.svg" style={{ width: "10%", height: "10%" }}></img>
                         <p className='text-sm md:text-xl lg:text-3xl font-bold'>Code Atoms</p>
