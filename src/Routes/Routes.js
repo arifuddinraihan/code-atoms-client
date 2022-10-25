0c9a9a53e4a9b9d860f3e1fd819b6888efaa5fc4
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-25 17:16:03 +0600

    404 page added with lottiefiles

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,13 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog/Blog";
 import Faq from "../components/FAQ/Faq";
 import Main from "../Layout/Main";
-import Login from "../Login/Login";
-import Register from "../Login/Register";
+import Login from "../components/Login/Login";
+import Register from "../components/Login/Register";
+import ErrorPage from "../components/Shared/ErrorPage/ErrorPage";
 
 export const routes = createBrowserRouter([
     {
         path : '/',
         element : <Main></Main>,
+        errorElement : <ErrorPage></ErrorPage>,
         children : [
             {
                 path : '/courses'
---
+++ src/components/Shared/ErrorPage/ErrorPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import { Link } from 'react-router-dom';
+const ErrorPage = () => {
+    return (
+        <div className='mx-auto flex flex-col justify-center'>
+            <div className="hero min-h-screen bg-transparent">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <Player src={'https://assets2.lottiefiles.com/packages/lf20_m6k6uhad.json'}
+                        className="player"
+                        loop
+                        autoplay
+                    ></Player>
+                    <div className=''>
+                        <h1 className="text-5xl font-bold mt-4 lg:mt-0 text-secondary hover:text-cyan-800">404</h1>
+                        <p className="py-6">Sorry we couldn't find
+                            <span className='px-2 text-cyan-600 text-2xl'>this page.</span></p>
+                        <Link to={'/'} className=' flex gap-3'>
+                            <button class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-cyan-500 active:bg-cyan-900 hover:bg-cyan-400">back to homepage</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className=''>
+
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage
