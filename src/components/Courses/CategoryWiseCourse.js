3ddadbc83ca458fa7a4b6951c7bac886b7c353e1
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-26 13:53:51 +0600

    added dynamic course image, title, details, review, review count in card

--- src/components/Courses/CategoryWiseCourse.js
+++ src/components/Courses/CategoryWiseCourse.js
@@ -4,12 +4,15 @@ import Course from './Course';
 
 const CategoryWiseCourse = () => {
     const catWiseCourse = useLoaderData()
-
+    console.log(catWiseCourse)
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
 
             {
-                catWiseCourse.map(singleCourse => <Course></Course>)
+                catWiseCourse.map(singleCourse => <Course 
+                    key={singleCourse.id}
+                    singleCourse={singleCourse}
+                ></Course>)
             }
             
         </div>
--- src/components/Courses/Course.js
+++ src/components/Courses/Course.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
 
-const Course = () => {
+const Course = ({ singleCourse }) => {
+    const { category, description, image, title, rating } = singleCourse
+    const { count, rate } = rating
     return (
         <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <img className="object-cover w-full h-64" src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Article" />
+            <img className="object-contain w-full h-64" 
+            src={image} alt={title} />
 
-                <div className="p-6">
-                    <div>
-                        <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">Product</span>
-                        <a href="#" className="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline" tabindex="0" role="link">I Built A Successful Blog In One Year</a>
-                        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec senectus erat. Et malesuada lobortis.</p>
-                    </div>
+            <div className="p-6">
+                <div>
+                    <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">{category}</span>
+                    <a href="#" className="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline" tabindex="0" role="link">{title ? title.slice(0, 60)+"..." : "N/A"}</a>
+                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                        {description ? description.slice(0, 100)+"..." : "N/A"}
+                    </p>
+                </div>
 
-                    <div className="mt-4">
+                <div className="mt-4">
+                    <div className="flex justify-between items-center">
                         <div className="flex items-center">
-                            <div className="flex items-center">
-                                <img className="object-cover h-10 rounded-full" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60" alt="Avatar"/>
-                                    <a href="#" className="mx-2 font-semibold text-gray-700 dark:text-gray-200" tabindex="0" role="link">Jone Doe</a>
-                            </div>
-                            <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">21 SEP 2015</span>
+                            <FaStar></FaStar>
+                            <a href="#" className="mx-2 font-semibold text-gray-700 dark:text-gray-200" tabindex="0" role="link">{rate}</a>
                         </div>
+                        <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">Reviews : {count}</span>
                     </div>
                 </div>
+            </div>
         </div>
     );
 };
